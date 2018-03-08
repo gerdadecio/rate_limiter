e533cd204e7b5ae77d1793d03bf413c05090ee2f
Gerda Decio <>
2018-03-09 02:12:12 +0800

    Rate limit class in lib/middleware

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative '../lib/middleware/rate_limit'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,5 +16,6 @@ module RateLimiter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.use Middleware::RateLimit, Redis.new(host: 'redis', port:  6379)
   end
 end
